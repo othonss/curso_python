79758f5ac19be5e2f35fa2e7a9db273c1ed0bdc8
othonss <othon.santos.silva@gmail.com>
2026-05-26 20:10:57 -0300

    Métodos para ler e escrever dentro de arquivos.

--- criando_arquivos_com_python/criado.py
+++ criando_arquivos_com_python/criado.py
@@ -4,3 +4,12 @@ caminho_arquivo += 'arquivo_criado.txt'
 
 with open(caminho_arquivo, 'w') as arquivo:
     print('Olá, este é um arquivo criado com Python!')
+    arquivo.write('Linha 1\n')
+    arquivo.write('Linha 2\n')
+    # O método writelines() escreve uma lista de strings no arquivo, sem adicionar quebras de linha automaticamente, por isso é necessário incluir '\n' no final de cada string para garantir que cada linha seja escrita em uma nova linha no arquivo.
+    arquivo.writelines(['Linha 3\n', 'Linha 4\n', 'Linha 5\n'])
+
+with open(caminho_arquivo, 'r') as arquivo:
+    print('Lendo o arquivo criado:')
+    conteudo = arquivo.read()
+    print(conteudo)
