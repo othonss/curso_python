fd868992719087bad5f96a4dda818012413cb13c
othonss <othon.santos.silva@gmail.com>
2026-05-28 19:11:33 -0300

    estudo: uso de property setter e também a aplicação da convenção utilizada em Python equivalente a atributos protegidos ou privados em outras linguagens com o _ (ex: _atributo)

--- orientacao_objetos/video_214_property.py
+++ orientacao_objetos/video_214_property.py
@@ -1,18 +1,34 @@
 class Caneta:
     def __init__(self, cor):
-        self.cor_tinta = cor
+        #Convenção para dizer que é privado ou protegido
+        self._cor = cor
+        self._cor_tampa = None
 
     def get_cor(self):
-        print('Get cor')
-        return self.cor_tinta
+        return self._cor
 
     #Tanto o get_cor quanto o property são equivalente neste caso
     @property
     def cor(self):
-        print('Property')
-        return self.cor_tinta
+        return self._cor
+    
+    @cor.setter
+    def cor(self, valor):  
+        self._cor = valor
+
+    @property
+    def cor_tampa(self):
+        return self._cor_tampa
+    
+    @cor_tampa.setter
+    def cor_tampa(self, valor):
+        self._cor_tampa = valor
    
 
 caneta = Caneta('Azul')
 print(caneta.get_cor())
 print(caneta.cor)
+caneta.cor = 'Rosa'
+caneta.cor_tampa = 'Preta'
+print(caneta.cor)
+print(caneta.cor_tampa)
