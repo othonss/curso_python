e4670fed3338e854fb8f575b5bca1f64f0096a06
othonss <othon.santos.silva@gmail.com>
2026-05-28 14:57:45 -0300

    Criando classes com Python

---
+++ orientacao_objetos/video_200_classes.py
@@ -0,0 +1,16 @@
+class Pessoa:
+    ...
+
+p1 = Pessoa()
+p1.nome = 'Othon'
+p1.sobrenome = 'Santos'
+
+p2 = Pessoa()
+p2.nome = 'Thais'
+p2.sobrenome = 'Santos'
+
+print(p1.nome)
+print(p1.sobrenome)
+print()
+print(p2.nome)
+print(p2.sobrenome)
