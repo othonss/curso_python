7f6cf341185395b35b7b7a0a38b9b6f3655dc8f7
othonss <othon.santos.silva@gmail.com>
2026-05-29 12:00:31 -0300

    prática: Exercício para compreender melhor as relações de classes

---
+++ orientacao_objetos/exercicio_video_220.py
@@ -0,0 +1,45 @@
+class Carro:
+    def __init__(self, nome):
+        self.nome = nome
+        self._motor = None
+        self._fabricante = None
+    
+    @property
+    def motor(self):
+        return self._motor
+    
+    @motor.setter
+    def motor(self, valor):
+        self._motor = valor
+
+    @property
+    def fabricante(self):
+        return self._fabricante
+    
+    @fabricante.setter
+    def fabricante(self, valor):
+        self._fabricante = valor
+
+class Motor:
+    def __init__(self, nome):
+        self.nome = nome
+    
+
+    
+class Fabricante:
+    def __init__(self, nome):
+        self.nome = nome
+
+fusca = Carro('Fusca')
+volkswagen = Fabricante('Volkswagen')
+motor_1_0 = Motor('1.0')
+fusca.fabricante = volkswagen
+fusca.motor = motor_1_0
+print(fusca.nome, fusca.fabricante.nome, fusca.motor.nome)
+
+celta = Carro('Celta')
+chevrolet = Fabricante('Chevrolet')
+motor_1_4 = Motor('1.4')
+celta.fabricante = chevrolet
+celta.motor = motor_1_4
+print(celta.nome, celta.fabricante.nome, celta.motor.nome)
