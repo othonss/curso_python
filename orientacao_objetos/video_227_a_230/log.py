79cb55b0b3293b66f00c65ca1152ee17a47c8390
othonss <othon.santos.silva@gmail.com>
2026-05-29 16:15:17 -0300

    estudo: Foram criadas outras classes para aplicar o conceito de herança múltipla

--- orientacao_objetos/video_227_a_230/log.py
+++ orientacao_objetos/video_227_a_230/log.py
@@ -1,4 +1,8 @@
 #Superclasse
+from pathlib import Path
+
+LOG_FILE = Path(__file__).parent / 'log.txt'
+
 class Log:
     def _log(self, msg):
         raise NotImplementedError('Implemente o método log')
@@ -14,13 +18,20 @@ class Log:
 
 class LogFileMixin(Log):
     def _log(self, msg):
-        print(msg)
+        msg_formatada = f'{msg} ({self.__class__.__name__})'
+        print('Salvando no log: ', msg_formatada)
+        with open(LOG_FILE, 'a', encoding='utf-8') as arquivo:
+            arquivo.write(msg_formatada)
+            arquivo.write('\n')
 
 class LogPrintMixin(Log):
     def _log(self, msg):
         print(f'{msg} ({self.__class__.__name__})')
 
 if __name__ == '__main__':    
-    l = LogPrintMixin()
-    l.log_error('Qualquer coisa')
-    l.log_success('Que legal')
+    lp = LogPrintMixin()
+    lp.log_error('Qualquer coisa')
+    lp.log_success('Que legal')
+    lf = LogFileMixin()
+    lf.log_error('Qualquer coisa')
+    lf.log_success('Que legal')
