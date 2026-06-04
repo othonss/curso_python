7f1de882eba0fc1c7911b313f737d498003d06b0
othonss <othon.santos.silva@gmail.com>
2026-06-04 19:36:01 -0300

    estudo: namedtuple pode ser usado com classe também

--- orientacao_objetos/video_273_namedtuple.py
+++ orientacao_objetos/video_273_namedtuple.py
@@ -1,6 +1,11 @@
-from collections import namedtuple
+from typing import NamedTuple
+#from collections import namedtuple
 
-Carta = namedtuple('Carta', ['valor', 'naipe'], defaults=['', ''])
+#Carta = namedtuple('Carta', ['valor', 'naipe'], defaults=['', ''])
+
+class Carta(NamedTuple):
+    valor: str = ''
+    naipe: str = ''
 as_espadas = Carta('A', 'Espada')
 print(as_espadas)
 print(as_espadas.valor)
