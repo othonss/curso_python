989b5901d3ead05cf4fda8e3cd17bbccd07567f5
othonss <othon.santos.silva@gmail.com>
2026-06-04 19:11:55 -0300

    estudo: post init no Python é algo que será executado após o init

--- orientacao_objetos/video_267_data_classes.py
+++ orientacao_objetos/video_267_data_classes.py
@@ -5,12 +5,16 @@ class Pessoa:
     nome: str
     sobrenome: str
 
-    @property
-    def nome_completo(self):
-        return f'{self.nome} {self.sobrenome}'
+    def __post_init__(self):
+        self.nome_completo = f'{self.nome} {self.sobrenome}'
+
+
+    # @property
+    # def nome_completo(self):
+    #     return f'{self.nome} {self.sobrenome}'
 
 
 if __name__ == '__main__':
     p1 = Pessoa('Luiz', 'Otávio')
     print(p1)
-    print(p1.nome_completo())
+    print(p1.nome_completo)
